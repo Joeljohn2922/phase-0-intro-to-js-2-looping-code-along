4797f22cd69f45ddeb63737683ef24e7f39d4098
Joel John <johnjoel079@gmail.com>
2024-03-29 17:09:45 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+   // console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+ // } 
+
+ function writeCards(arrayOfNames,eventName){ 
+    const messages=[]
+    for(let i = 0; i< arrayOfNames. length; i++){
+        console.log(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+        messages.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+return messages 
+} 
+
+ 
+
+function countDown(number){ 
+
+    while(number >= 0){ 
+        console.log(number--)
+    }
+}
